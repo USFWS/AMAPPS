af8e8a6a5b7df8075624f9f6cf0dae44233e2b21
Kaycee Coleman <kaycee.coleman@gmail.com>
2018-06-14 16:20:24 -0400

    Update SarahYates_DCCO_June2018.R

--- NWASC/data_requests/SarahYates_DCCO_June2018.R
+++ NWASC/data_requests/SarahYates_DCCO_June2018.R
@@ -20,6 +20,9 @@ tracks.in.db = sqlQuery(db, paste("select * from track where dataset_id = 139"))
 obs.in.db = sqlQuery(db, paste("select * from observation 
                                where dataset_id = 139 
                                and spp_cd = 'DCCO'"))
+obs.in.db = rename(obs.in.db, longitude = temp_lon, latitude = temp_lat) %>%
+  mutate(obs_count_general_nb = ifelse(!is.na(transect_id), NA, obs_count_general_nb),
+         obs_count_intrans_nb = ifelse(is.na(transect_id), NA, obs_count_intrans_nb))
 # ------------ # 
 
 
@@ -36,6 +39,11 @@ transects.in.db = transects.in.db %>% filter(transect_id %in% transect.list)
 # export
 # ------------ # 
 write.csv(obs.in.db, "//ifw-hqfs1/MB SeaDuck/seabird_database/data_sent/SarahYates_DCCO_June2018/DCCO_AMAPPS_Jan2014.csv")
-write.csv(obs.in.db, "//ifw-hqfs1/MB SeaDuck/seabird_database/data_sent/SarahYates_DCCO_June2018/tracks_AMAPPS_Jan2014.csv")
-write.csv(obs.in.db, "//ifw-hqfs1/MB SeaDuck/seabird_database/data_sent/SarahYates_DCCO_June2018/tracks_AMAPPS_Jan2014.csv")
+write.csv(tracks.in.db, "//ifw-hqfs1/MB SeaDuck/seabird_database/data_sent/SarahYates_DCCO_June2018/tracks_AMAPPS_Jan2014.csv")
+write.csv(transects.in.db, "//ifw-hqfs1/MB SeaDuck/seabird_database/data_sent/SarahYates_DCCO_June2018/transects_AMAPPS_Jan2014.csv")
 # ------------ # 
+
+
+# plot
+require(ggplot2)
+ggplot()+geom_point(data = obs.in.db, aes(x=longitude, y=latitude, size = obs_count_intrans_nb))
