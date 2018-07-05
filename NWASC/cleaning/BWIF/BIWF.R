38927686042d62f68b491f4c080958acc8c34c89
Kaycee Coleman <kaycee.coleman@gmail.com>
2018-07-05 16:34:44 -0400

    Update BIWF.R

--- NWASC/cleaning/BWIF/BIWF.R
+++ NWASC/cleaning/BWIF/BIWF.R
@@ -30,8 +30,13 @@ dir.out <- paste(gsub("datasets_received", "data_import/in_progress", dir), surv
 # load boat data and transects
 # -------------------------------- #
 data = read_excel(paste(dir.in,"2016-11-09_BIWF OFFSHORE AVIAN DATA 0709 - 0610.xls",sep="/"))
+
+ofs.data = odbcConnectExcel2007(paste(dir.in,"4.5 BIWF_Tables_OFS_PHYLO.xlsx",sep="/"))
+ofs.avian = sqlFetch(ofs.data, "OFFSHORE AVIAN")
+
 hd.data = read_excel(paste(dir.in,"2016-11-09_BIWF Full Year HD Aerial Data.xlsx",sep="/"), 
                      sheet = "12 month HD data Avian Only")
+
 ons.data = odbcConnectExcel2007(paste(dir.in,"2016-11-09_BIWF ONS 0709 - 0610.xlsx",sep="/"))
 on.avian = sqlFetch(ons.data, "DATA - ONSHORE AVIAN")
 on.weather = sqlFetch(ons.data, "ONSHORE WEATHER")
@@ -63,7 +68,7 @@ trans = readOGR(dir.in,'BIWF_Offshore_Boat-Based_Avian_Survey_Transects')
 #bat.on.pass = readOGR(dir.in,'BIWF_Onshore_Passive_Bat_Sampling_Points')
 #raptor.on = readOGR(dir.in,'BIWF_Onshore_Raptor_Migration_Survey_Points')
 #avian.on.cov = readOGR(dir.in,'BIWF_Onshore_Sea_Watch_Avian_Survey_Coverage')
-#avian.on.surv.pt = readOGR(dir.in,'BIWF_Onshore_Sea_Watch_Avian_Survey_Points')
+avian.on.surv.pt = readOGR(dir.in,'BIWF_Onshore_Sea_Watch_Avian_Survey_Points')
 # -------------------------------- #
 
 
@@ -74,33 +79,31 @@ names(on.avian) = gsub(" ", "", names(on.avian), fixed = TRUE)
 on.avian = on.avian %>%
   rename(count = '#') %>% 
   mutate(original_species_code = paste(SPECIESNAME, SPECIESCODE, sep = "; "),
-         lat = NA, 
-         lon = NA, 
-         lat = ifelse(POINT == 1, 41.200012, lat),
-         lon = ifelse(POINT == 1, -71.574038, lon),
-         lat = ifelse(POINT == 2, 41.189686, lat),
-         lon = ifelse(POINT == 2, -71.566494, lon),
-         lat = ifelse(POINT == 3, 41.172362, lat),
-         lon = ifelse(POINT == 3, -71.554446, lon),
-         lat = ifelse(POINT == 4, 41.165931, lat),
-         lon = ifelse(POINT == 4, -71.59296, lon),
-         lat = ifelse(POINT == 5, 41.152282, lat),
-         lon = ifelse(POINT == 5, -71.552535, lon),
-         lat = ifelse(POINT == 7, 41.151212, lat),
-         lon = ifelse(POINT == 7, -71.558179, lon),
-         lat = ifelse(POINT == 6, 41.151704, lat),
-         lon = ifelse(POINT == 6, -71.555504, lon),
-         lat = ifelse(POINT == 8, 41.148526, lat),
-         lon = ifelse(POINT == 8, -71.575857, lon),
-         lat = ifelse(POINT == 9, 41.147455, lat),
-         lon = ifelse(POINT == 9, -71.590879, lon),
-         lat = ifelse(POINT == 10, 41.151666, lat),
-         lon = ifelse(POINT == 10, -71.608702, lon),
-         DISTANCE = ifelse(DISTANCE %in% 1, "0 - 500 m", ifelse(DISTANCE %in% 2, "500 - 1500 m", "1500 - 3000 m")), 
-         admin_notes= "Latitude and Longitude estimated based on report map",
+         SPECIESNAME = tolower(SPECIESNAME),
+          DISTANCE = ifelse(DISTANCE %in% 1, "0 - 500 m", ifelse(DISTANCE %in% 2, "500 - 1500 m", "1500 - 3000 m")), 
          STARTTIME = format(on.avian$STARTTIME, format = "%H:%M:%S"), 
          ENDTIME = format(on.avian$ENDTIME, format = "%H:%M:%S"),
          OBSTIME = format(on.avian$OBSTIME, format = "%H:%M:%S"))
+
+avian.on.surv.pt = spTransform(avian.on.surv.pt, CRS("+proj=longlat +datum=WGS84")) #change projection
+avian.on.surv.pt = as.data.frame(avian.on.surv.pt) %>% rename(lat = coords.x2, lon = coords.x1)
+
+on.avian = left_join(on.avian, dplyr::select(avian.on.surv.pt, Point, lat, lon), by = c("POINT" = "Point"))
+rm(avian.on.surv.pt)
+# ------- #
+
+# ------- #
+names(ofs.avian) = gsub(" ", "", names(ofs.avian), fixed = TRUE)
+ofs.avian = ofs.avian %>%
+  rename(count = '#') %>% 
+  mutate(original_species_code = paste(SPECIESNAME, SPECIESCODE, sep = "; "),
+         SPECIESNAME = tolower(SPECIESNAME),
+         DISTANCE = ifelse(DISTANCE %in% 1, "0 - 500 m", ifelse(DISTANCE %in% 2, "500 - 1500 m", "1500 - 3000 m")), 
+         STARTTIME = format(STARTTIME, format = "%H:%M:%S"), 
+         ENDTIME = format(ENDTIME, format = "%H:%M:%S"),
+         OBSTIME = format(on.avian$OBSTIME, format = "%H:%M:%S"),
+         Time = format(Time, format = "%H:%M:%S"))
+# ------- #
 # -------------------------------- #
 
 
@@ -110,8 +113,39 @@ on.avian = on.avian %>%
 db <- odbcDriverConnect('driver={SQL Server}; server=ifw-dbcsqlcl1.fws.doi.net; database=NWASC; trusted_connection=true')
 spplist <- sqlFetch(db, "lu_species")
 odbcClose(db)
-spplist = mutate(spplist, common_name = tolower(common_name)) %>% filter(common_name, spp_cd)
+spplist = mutate(spplist, common_name = tolower(common_name)) %>% dplyr::select(common_name, spp_cd)
 
-on.avian = left_join(on.avian, spplist, by = c("SPECIESNAME" = "common_name")) 
+# ------- #
+on.avian = left_join(on.avian, spplist, by = c("SPECIESNAME" = "common_name")) %>%
+  mutate(spp_cd = replace(spp_cd, original_species_code %in% "American golden-plover; AMGP","AMGP"),
+         spp_cd = replace(spp_cd, original_species_code %in% "Unidentified accipiter; UNAC", "UAHA"),
+         spp_cd = replace(spp_cd, original_species_code %in% "Unidentified Larus gull; ULGU","UNGU")) %>% 
+  dplyr::select(-SPECIESCODE, -SPECIESNAME, -MONTH)
+# ------- #
+
+# ------- #
+ofs.avian = left_join(ofs.avian, spplist, by = c("SPECIESNAME" = "common_name")) %>%
+  mutate(spp_cd = replace(spp_cd, original_species_code %in% "American golden-plover; AMGP","AMGP"),
+         spp_cd = replace(spp_cd, original_species_code %in% "Unidentified accipiter; UNAC", "UAHA"),
+         spp_cd = replace(spp_cd, original_species_code %in% "Unidentified Larus gull; ULGU","UNGU")) %>% 
+  dplyr::select(-SPECIESCODE, -SPECIESNAME, -MONTH)
+# ------- #
 # -------------------------------- #
 
+
+# -------------------------------- #
+# effort
+# -------------------------------- #
+on.avian.transect = on.avian %>% group_by(POINT, DATE) %>% 
+  summarize(start_time = first(STARTTIME), end_time = first(ENDTIME),
+            start_lat = first(lat), end_lat = first(lat),
+            start_lon = first(lon), end_lon = first(lon))
+
+# ------- #
+# ofs.avian.transect 
+# trans
+# trans.segs
+trans.segs2 = spTransform(trans.segs, CRS("+proj=longlat +datum=WGS84")) #change projection
+trans.segs2 = as.data.frame(trans.segs2) 
+
+# -------------------------------- #
