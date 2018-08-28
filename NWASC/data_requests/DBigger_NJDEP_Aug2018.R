17cdf42f9f1a6e3e2f222edb8d5695d9197a3abb
Kaycee Coleman <kaycee.coleman@gmail.com>
2018-08-28 13:09:18 -0400

    Update DBigger_NJDEP_Aug2018.R

--- NWASC/data_requests/DBigger_NJDEP_Aug2018.R
+++ NWASC/data_requests/DBigger_NJDEP_Aug2018.R
@@ -9,17 +9,27 @@
 require(odbc)
 require(RODBC)
 require(dplyr)
+require(rgdal)
 # -------------- #
 
 # -------------- #
 # data
 # -------------- #
+# in database
 db <- dbConnect(odbc::odbc(),driver='SQL Server',server='ifw9mbmsvr008', database='SeabirdCatalog')
 dates <- dbGetQuery(db, "select distinct obs_dt, platform_tx from observation where dataset_id = 91")
 
 db <- odbcDriverConnect('driver={SQL Server}; server=ifw-dbcsqlcl1.fws.doi.net; database=NWASC; trusted_connection=true')
 ds <- sqlQuery(db, "select * from dataset where dataset_id = 91")
 ppl <- sqlQuery(db, "select * from lu_people where user_id = 56")
+
+# raw data
+fgdb <- "//ifw-hqfs1/MB SeaDuck/seabird_database/datasets_received/NewJerseyDEP/GIS/Layers/NJDEP Layers.gdb"
+
+# List all feature classes in a file geodatabase
+subset(ogrDrivers(), grepl("GDB", name))
+fc_list <- ogrListLayers(fgdb)
+print(fc_list)
 # -------------- #
 
 # -------------- #
