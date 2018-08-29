714a8fcba3f8eb7089ce6a6dd913ba35d8b36c76
Kaycee Coleman <kaycee.coleman@gmail.com>
2018-08-29 12:45:49 -0400

    Update NJDEP_aerial_2008.R

--- NWASC/cleaning/NJDEP_2008/NJDEP_aerial_2008.R
+++ NWASC/cleaning/NJDEP_2008/NJDEP_aerial_2008.R
@@ -23,10 +23,24 @@ fc_list <- ogrListLayers(fgdb)
 print(fc_list)
 
 # Read the feature class
-dat <- readOGR(dsn=fgdb,layer="AerialApr2008Bird")
+dat.effort <- readOGR(dsn=fgdb,layer="AerialApr2008Bird")
+dat.obs <- readOGR(dsn=fgdb,layer="AvianSurveySightings") # only boat data, check if all was put in, including Supplemental
+dat.boats <- readOGR(dsn=fgdb,layer="Commercial_Vessels_Aerial") #might need to add these data
+dat.mm <- readOGR(dsn=fgdb,layer="MammalTurtleSightings_CompleteAerialSurveyData") #might need to add
+#dat.obs <- readOGR(dsn=fgdb,layer="ESM_Avian") # environmental sensitivity index map
+#dat.obs <- readOGR(dsn=fgdb,layer="AerialApr2008oneffort") # not the right line pattern
 
-# Determine the FC extent, projection, and attribute information
-summary(dat)
+# fauna geodatabase
+# fgdb <- "//ifw-hqfs1/MB SeaDuck/seabird_database/datasets_received/NewJerseyDEP/GIS/Layers/fauna.zip/fauna.gdb"
+# 
+# # List all feature classes in a file geodatabase
+# subset(ogrDrivers(), grepl("GDB", name))
+# fc_list <- ogrListLayers(fgdb)
+# print(fc_list)
+# -------------- #
 
-# View the feature class
-plot(dat)
+
+# -------------- #
+# format
+# -------------- #
+# -------------- #
