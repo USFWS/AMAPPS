0fa6bb7192583ab1f02d827d632bec769de2a07e
Kaycee Coleman <kaycee.coleman@gmail.com>
2018-12-12 17:05:59 -0500

    Create AGilbert_DE_DOEMidA_Dec2018.R

---
+++ NWASC/data_requests/AGilbert_DE_DOEMidA_Dec2018.R
@@ -0,0 +1,63 @@
+# A. Gilbert, requested 12/10/18
+#
+# 1) DOE Mid-Atlantic hidef aerial and boat survey data. I know, this is 
+# our data, but I want it in all it's glory from the Seabird Catalog.
+# 
+# 2) All data for all species from 37.3-39.3 N lat. obs and effort.
+
+# ------------------ # 
+# load packages
+# ------------------ # 
+require(dplyr)
+require(ggplot2)
+require(RODBC)
+require(odbc)
+library(readr)
+library(readxl)
+# ------------------ # 
+
+
+# ------------------ #
+# load data
+# ------------------ #
+db <- dbConnect(odbc::odbc(),driver='SQL Server',server='ifw-dbcsqlcl1',database='NWASC')
+datasets = dbGetQuery(db, "select * from dataset")
+lit = dbGetQuery(db, "select * from links_and_literature")
+spp = dbGetQuery(db, "select * from lu_species")
+#obs = dbGetQuery(db, "select * from observations")
+dbDisconnect(db)
+
+# obs data (temp)
+obs <- read_csv("Z:/seabird_database/data_sent/TWhite_archive_Oct2018/obs_Oct2018.csv")
+obs_bb = obs %>% filter(latitude >= 37.3, latitude <= 39.3)
+ind = datasets %>% filter(parent_id %in% c(8,9))
+obs_doe_midA = obs %>% filter(dataset_id %in% ind)
+# ------------------ #
+
+# ------------------ #
+# formatting
+# ------------------ #
+# filter datasets
+datasets = filter(datasets, dataset_id %in% obs_bb$dataset_id, !share_level_id %in% 1)
+obs_bb = filter(obs_bb, dataset_id %in% datasets$dataset_id)
+
+# transform datasets
+source("Z://seabird_database/Rfunctions/transformDatasets.R")
+summaries = read_excel("Z:/seabird_database/documentation/How to and Reference files/NWASC_guidance/dataset_summaries_Aug2018.xlsx")
+summaries = summaries[,1:5] %>% dplyr::select(-source_dataset_id) 
+names(summaries) = tolower(names(summaries))
+
+datasets = transformDataset(datasets) %>% 
+  dplyr::select(-data_url, -report, -data_citation, -publications, 
+                -publication_url, -publication_DOI, -id,
+                -dataset_summary, -dataset_processing, -dataset_quality) %>%
+  left_join(., lit, by="dataset_id") %>% 
+  left_join(., summaries, by="dataset_id")
+
+# export
+write.csv(obs_midA,"Z:/seabird_database/data_sent/AGilbert_DE_DOEMidA_Dec2018/hidef_observations.csv")
+write.csv(obs_bb,"Z:/seabird_database/data_sent/AGilbert_DE_DOEMidA_Dec2018/DE_observations.csv")
+write.csv(cc.datasets,"Z:/seabird_database/data_sent/AGilbert_DE_DOEMidA_Dec2018/datasets.csv")
+# ------------- #
+
+
