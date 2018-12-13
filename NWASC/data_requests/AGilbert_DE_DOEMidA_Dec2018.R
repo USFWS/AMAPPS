2a3802b2b77ebdd6f57ad7df60eb3023bab9f85a
Kaycee Coleman <kaycee.coleman@gmail.com>
2018-12-13 16:41:34 -0500

    Update AGilbert_DE_DOEMidA_Dec2018.R

--- NWASC/data_requests/AGilbert_DE_DOEMidA_Dec2018.R
+++ NWASC/data_requests/AGilbert_DE_DOEMidA_Dec2018.R
@@ -25,13 +25,15 @@ datasets = dbGetQuery(db, "select * from dataset")
 lit = dbGetQuery(db, "select * from links_and_literature")
 spp = dbGetQuery(db, "select * from lu_species")
 #obs = dbGetQuery(db, "select * from observations")
+DE_effort = dbGetQuery(db, "select * from all_effort where latitude >= 37.3 and latitude <= 39.3")
+midA_effort = dbGetQuery(db, "select * from all_effort where dataset_id %in% (114,115,124,125,126,127,128,129,130,148,150,151,152,153,154,155,156,157,168)")
 dbDisconnect(db)
 
 # obs data (temp)
 obs <- read_csv("Z:/seabird_database/data_sent/TWhite_archive_Oct2018/obs_Oct2018.csv")
 obs_bb = obs %>% filter(latitude >= 37.3, latitude <= 39.3)
-ind = datasets %>% filter(parent_id %in% c(8,9))
-obs_doe_midA = obs %>% filter(dataset_id %in% ind)
+ind = datasets %>% filter(parent_project %in% c(8,9))
+obs_doe_midA = obs %>% filter(dataset_id %in% ind$dataset_id)
 # ------------------ #
 
 # ------------------ #
@@ -49,15 +51,15 @@ names(summaries) = tolower(names(summaries))
 
 datasets = transformDataset(datasets) %>% 
   dplyr::select(-data_url, -report, -data_citation, -publications, 
-                -publication_url, -publication_DOI, -id,
+                -publication_url, -publication_DOI, 
                 -dataset_summary, -dataset_processing, -dataset_quality) %>%
   left_join(., lit, by="dataset_id") %>% 
   left_join(., summaries, by="dataset_id")
 
 # export
-write.csv(obs_midA,"Z:/seabird_database/data_sent/AGilbert_DE_DOEMidA_Dec2018/hidef_observations.csv")
+write.csv(obs_doe_midA,"Z:/seabird_database/data_sent/AGilbert_DE_DOEMidA_Dec2018/DOE_MidA_hidef_observations.csv")
 write.csv(obs_bb,"Z:/seabird_database/data_sent/AGilbert_DE_DOEMidA_Dec2018/DE_observations.csv")
-write.csv(cc.datasets,"Z:/seabird_database/data_sent/AGilbert_DE_DOEMidA_Dec2018/datasets.csv")
+write.csv(datasets,"Z:/seabird_database/data_sent/AGilbert_DE_DOEMidA_Dec2018/datasets.csv")
 # ------------- #
 
 
