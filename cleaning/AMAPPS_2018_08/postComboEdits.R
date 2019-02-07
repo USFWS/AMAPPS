5f92c1459c64057973094e2668710368dab29e6a
Kaycee Coleman <kaycee.coleman@gmail.com>
2019-02-07 16:00:41 -0500

    Update postComboEdits.R

--- cleaning/AMAPPS_2018_08/postComboEdits.R
+++ cleaning/AMAPPS_2018_08/postComboEdits.R
@@ -15,28 +15,63 @@ testplot <- function(t,s,x,y){
 #------------#
 
 # 410100
-obstrack$transect[obstrack$seat %in% "rf" & obstrack$transect %in% 410100 & obstrack$sec > 41363.95 & obstrack$sec < 45801.86] = NA
-obstrack$offline[obstrack$seat %in% "rf" & obstrack$transect %in% 410100 & obstrack$sec> 41363.95 & obstrack$sec < 45801.86] = 1
-obstrack$transect[obstrack$seat %in% "lf" & obstrack$transect %in% 410100 & obstrack$sec > 41340.67 & obstrack$sec < 45779.77] = NA
-obstrack$offline[obstrack$seat %in% "lf" & obstrack$transect %in% 410100 & obstrack$sec> 41340.67 & obstrack$sec < 45779.77] = 1
+t = 410100
+s = "rf"
+x = filter(obstrack, seat %in% s, transect %in% t) %>% arrange(sec)
+y = filter(x,type %in% c("BEGCNT","ENDCNT"))
+testplot(t,s,x,y)
+obstrack$transect[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > 41363.95 & obstrack$sec < 45801.86] = NA
+obstrack$offline[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec> 41363.95 & obstrack$sec < 45801.86] = 1
+s = "lf"
+x = filter(obstrack, seat %in% s, transect %in% t) %>% arrange(sec)
+y = filter(x,type %in% c("BEGCNT","ENDCNT"))
+testplot(t,s,x,y)
+obstrack$transect[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > 41340.67 & obstrack$sec < 45779.77] = NA
+obstrack$offline[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec> 41340.67 & obstrack$sec < 45779.77] = 1
 
 # 395100
-obstrack$transect[obstrack$seat %in% "lf" & obstrack$transect %in% 395100 & obstrack$sec > 33498.24 & obstrack$sec < 33635.48] = NA
-obstrack$offline[obstrack$seat %in% "lf" & obstrack$transect %in% 395100 & obstrack$sec> 33498.24 & obstrack$sec < 33635.48] = 1
-obstrack$transect[obstrack$seat %in% "rf" & obstrack$transect %in% 395100 & obstrack$sec > 33522.66 & obstrack$sec < 33660.20] = NA
-obstrack$offline[obstrack$seat %in% "rf" & obstrack$transect %in% 395100 & obstrack$sec> 33522.66 & obstrack$sec < 33660.20] = 1
+t = 395100
+s = "lf"
+x = filter(obstrack, seat %in% s, transect %in% t) %>% arrange(sec)
+y = filter(x,type %in% c("BEGCNT","ENDCNT"))
+testplot(t,s,x,y)
+obstrack$transect[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > 33498.24 & obstrack$sec < 33635.48] = NA
+obstrack$offline[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec> 33498.24 & obstrack$sec < 33635.48] = 1
+s = "rf"
+x = filter(obstrack, seat %in% s, transect %in% t) %>% arrange(sec)
+y = filter(x,type %in% c("BEGCNT","ENDCNT"))
+testplot(t,s,x,y)
+obstrack$transect[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > 33522.66 & obstrack$sec < 33660.20] = NA
+obstrack$offline[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec> 33522.66 & obstrack$sec < 33660.20] = 1
 
 # 402100
-obstrack$transect[obstrack$seat %in% "rf" & obstrack$transect %in% 402100 & obstrack$sec > 38468.80 & obstrack$sec < 38636.16] = NA
-obstrack$offline[obstrack$seat %in% "rf" & obstrack$transect %in% 402100 & obstrack$sec> 38468.80 & obstrack$sec < 38636.16] = 1
-obstrack$transect[obstrack$seat %in% "lf" & obstrack$transect %in% 402100 & obstrack$sec > 38434.25 & obstrack$sec < 38612.82] = NA
-obstrack$offline[obstrack$seat %in% "lf" & obstrack$transect %in% 402100 & obstrack$sec> 38434.25 & obstrack$sec < 38612.82] = 1
+t = 402100
+s = "rf"
+x = filter(obstrack, seat %in% s, transect %in% t) %>% arrange(sec)
+y = filter(x,type %in% c("BEGCNT","ENDCNT"))
+testplot(t,s,x,y)
+obstrack$transect[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > 38468.80 & obstrack$sec < 38636.16] = NA
+obstrack$offline[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec> 38468.80 & obstrack$sec < 38636.16] = 1
+s = "lf"
+x = filter(obstrack, seat %in% s, transect %in% t) %>% arrange(sec)
+y = filter(x,type %in% c("BEGCNT","ENDCNT"))
+testplot(t,s,x,y)obstrack$transect[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > 38434.25 & obstrack$sec < 38612.82] = NA
+obstrack$offline[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec> 38434.25 & obstrack$sec < 38612.82] = 1
 
 # 380601
-obstrack$transect[obstrack$seat %in% "lf" & obstrack$transect %in% 380601 & obstrack$sec > 51921.85 & obstrack$sec < 52231.29] = NA
-obstrack$offline[obstrack$seat %in% "lf" & obstrack$transect %in% 380601 & obstrack$sec> 51921.85 & obstrack$sec < 52231.29] = 1
-obstrack$transect[obstrack$seat %in% "rf" & obstrack$transect %in% 380601 & obstrack$sec > 51947.05 & obstrack$sec < 52255.50] = NA
-obstrack$offline[obstrack$seat %in% "rf" & obstrack$transect %in% 380601 & obstrack$sec> 51947.05 & obstrack$sec < 52255.50] = 1
+t = 380601
+s = "lf"
+x = filter(obstrack, seat %in% s, transect %in% t) %>% arrange(sec)
+y = filter(x,type %in% c("BEGCNT","ENDCNT"))
+testplot(t,s,x,y)
+obstrack$transect[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > 51921.85 & obstrack$sec < 52231.29] = NA
+obstrack$offline[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec> 51921.85 & obstrack$sec < 52231.29] = 1
+s = "rf"
+x = filter(obstrack, seat %in% s, transect %in% t) %>% arrange(sec)
+y = filter(x,type %in% c("BEGCNT","ENDCNT"))
+testplot(t,s,x,y)
+obstrack$transect[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > 51947.05 & obstrack$sec < 52255.50] = NA
+obstrack$offline[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > 51947.05 & obstrack$sec < 52255.50] = 1
 
 # 363600
 t = 363600
@@ -154,23 +189,46 @@ s = "lf"
 x = filter(obstrack, seat %in% s, transect %in% t) %>% arrange(sec)
 y = filter(x,type %in% c("BEGCNT","ENDCNT"))
 testplot(t,s,x,y)
+# add BEG And END for when plane circles
+s1 = 45571.41
+s2 = 45686.43
+obstrack$type[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec %in% s1] = "ENDCNT"
+obstrack$type[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec %in% s2] = "BEGCNT" 
+obstrack$transect[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > s1 & obstrack$sec < s2] = NA
+obstrack$offline[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > s1 & obstrack$sec < s2] = 1
+
 s = "rf"
 x = filter(obstrack, seat %in% s, transect %in% t) %>% arrange(sec)
 y = filter(x,type %in% c("BEGCNT","ENDCNT"))
 testplot(t,s,x,y)
+# add BEG And END for when plane circles
+s1 = 45600.68
+s2 = 45715.82
+obstrack$type[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec %in% s1] = "ENDCNT"
+obstrack$type[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec %in% s2] = "BEGCNT" 
+obstrack$transect[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > s1 & obstrack$sec < s2] = NA
+obstrack$offline[obstrack$seat %in% s & obstrack$transect %in% t & obstrack$sec > s1 & obstrack$sec < s2] = 1
 
 # 334100
 t = 334100
-s = "lf"
+s = "rf"
 x = filter(obstrack, seat %in% s, transect %in% t) %>% arrange(sec)
 y = filter(x,type %in% c("BEGCNT","ENDCNT"))
 testplot(t,s,x,y)
 # moving end up closer to transect
+obstrack$dataChange[obstrack$transect %in% t & obstrack$seat %in% s & obstrack$type %in% "ENDCNT"] = paste(obstrack$dataChange[obstrack$transect %in% t & obstrack$seat %in% s & obstrack$type %in% "ENDCNT"],
+                                                                                                          "; changed TYPE to COMMENT since ENDCNT was offline ", sep = "")
+obstrack$type[obstrack$transect %in% t & obstrack$seat %in% s & obstrack$type %in% "ENDCNT"] = "COMMENT" 
+obstrack$dataChange[obstrack$transect %in% t & obstrack$seat %in% s & obstrack$sec %in% 55754.94] = paste(obstrack$dataChange[obstrack$transect %in% t & obstrack$seat %in% s & obstrack$sec %in% 55754.94],
+                                                                                                          "; changed TYPE to ENDCNT since ENDCNT was offline ", sep = "")
+obstrack$type[obstrack$transect %in% t & obstrack$seat %in% s & obstrack$sec %in% 55754.94] = "ENDCNT"
+obstrack$offline[obstrack$transect %in% t & obstrack$seat %in% s & obstrack$sec > 55754.94] = 1
+obstrack$transect[obstrack$transect %in% t & obstrack$seat %in% s & obstrack$sec > 55754.94] = NA
 
 # 35101 rf
 t = 351101
-s = "rf"
+s = "lf"
 x = filter(obstrack, seat %in% s, transect %in% t) %>% arrange(sec)
 y = filter(x,type %in% c("BEGCNT","ENDCNT"))
 testplot(t,s,x,y)
-# rf missing for 351101
+# rf missing for 351101 - was not in transit or original files 
